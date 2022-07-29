e950e33cd54b2c706442bc3d7551e27ce6f2495b
PinkiePie-tech <sebastien.pujol@gmail.com>
2022-07-29 10:44:09 +0200

    2 files modified

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,35 +13,53 @@ import {
   styleUrls: ['form.component.scss'],
 })
 export class FormComponent {
-  public formGroup = new FormGroup({
-    firstname: new FormControl(undefined, Validators.required),
-    name: new FormControl(undefined, Validators.required),
-    adress: new FormGroup(
-      {
-        city: new FormControl(),
-        code: new FormControl(),
-        street: new FormControl(),
-      },
-      Validators.required
-    ),
-    phone: new FormGroup(
-      {
-        prefix: new FormControl(undefined, Validators.maxLength(4)),
-        number: new FormControl(undefined, Validators.maxLength(10)),
-      },
-      Validators.required
-    ),
-    lead: new FormControl(undefined, [
-      this.LeadValidators,
-      Validators.required,
-    ]),
-  });
+  public formGroup = new FormGroup(
+    {
+      firstname: new FormControl(undefined, Validators.required),
+      name: new FormControl(undefined, Validators.required),
+      adress: new FormGroup(
+        {
+          city: new FormControl(undefined, Validators.required),
+          code: new FormControl(undefined, Validators.required),
+          street: new FormControl(undefined, Validators.required),
+        },
+        this.GroupValidators
+      ),
+      phone: new FormGroup(
+        {
+          prefix: new FormControl(undefined, [
+            Validators.maxLength(4),
+            Validators.required,
+          ]),
+          number: new FormControl(undefined, [
+            Validators.maxLength(10),
+            Validators.required,
+          ]),
+        },
+        this.GroupValidators
+      ),
+      lead: new FormControl(undefined, [
+        this.LeadValidators,
+        Validators.required,
+      ]),
+    },
+    this.GroupValidators
+  );
 
   private LeadValidators(control: AbstractControl): ValidationErrors {
     if (control?.value?.toLowerCase().search('jean')) {
       return {
         wrongLead: "Non c'est pas lui, roooh il y en a pas 40",
       };
+    }
+    return null;
+  }
+
+  private GroupValidators(group: FormGroup): ValidationErrors {
+    for (let key in group.controls) {
+      if (!!group.controls[key].errors) {
+        return group.controls[key].errors;
+      }
     }
     return null;
   }
